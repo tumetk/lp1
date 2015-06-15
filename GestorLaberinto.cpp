798160e9f8e57036bfa817064a8c4d476abd5a5a
Christian <Christian@192.168.1.35>
2015-06-15 14:35:21 -0500

    gestorlaberinto y laberinto...

---
+++ GestorLaberinto.cpp
@@ -0,0 +1,14 @@
+#include "GestorLaberinto.h"
+#include "Laberinto.h";
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+GestorLaberinto::crear(int numLaberinto, ...){
+//    Laberinto nodoLaberinto;
+//    ifstream inputFile("Laberinto1.txt");
+////    inputFile >> 
+//    
+}
