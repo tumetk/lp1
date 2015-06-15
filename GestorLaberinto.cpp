e1867a6d00201ee1bcd045caa316d4f2868b47b7
Christian <Christian@192.168.1.35>
2015-06-15 18:54:34 -0500

    gestorlaberinto y laberinto...

--- GestorLaberinto.cpp
+++ GestorLaberinto.cpp
@@ -1,14 +1,59 @@
-#include "GestorLaberinto.h"
-#include "Laberinto.h";
-#include <fstream>
+
+#include "GestorDeLaberinto.h"
+#include "Laberinto.h"
 #include <iostream>
-#include <cstring>
+#include <fstream>
 
 using namespace std;
 
-GestorLaberinto::crear(int numLaberinto, ...){
-//    Laberinto nodoLaberinto;
-//    ifstream inputFile("Laberinto1.txt");
-////    inputFile >> 
-//    
+GestorDeLaberinto::GestorDeLaberinto(int n) {
+    numLab = n;
+}
+
+GestorDeLaberinto::GestorDeLaberinto(const GestorDeLaberinto& orig) {
+}
+
+GestorDeLaberinto::~GestorDeLaberinto() {
+}
+
+Laberinto* GestorDeLaberinto::crear(){
+    ifstream inputFile("laberintos.txt",ios::in);
+    int M,N;
+    char car;
+    char linea[100];
+    Laberinto* laberintos = new Laberinto[numLab];
+    int numL = 0;
+    int numCar = 0;
+    inputFile >> M;
+    inputFile >> N;
+    inputFile.get();
+    while(!inputFile.eof()){
+        Laberinto laberinto(M,N);
+//        for(int i=0; i<N ; i++)
+//            for(int j=0;j<N;j++){
+//                car = inputFile.get();
+//                if(car != '\n')
+//                    laberinto.cargarCelda(i,j,car);
+//            }
+        for(int i=0; i<M ;i++){
+            inputFile.getline(linea,100);
+            for(int j=0;j<N;j++)
+                laberinto.cargarCelda(i,j,linea[j]);
+        }
+        laberinto.printLaberinto();
+        cout << endl<<endl;
+//        laberintos[numL++] = laberinto;
+        inputFile >> M;
+        inputFile >> N;
+        inputFile.get();
+    }    
+    return laberintos;
+}
+
+void GestorDeLaberinto::setNumLab(int numLab) {
+    this->numLab = numLab;
+}
+
+int GestorDeLaberinto::getNumLab() const {
+    return numLab;
 }
